8fc2b3a805b329f3aa39b68316bfffda8a612690
Muhammad Oshean Lehrmann <osheanman@berkeley.edu>
2023-05-24 22:52:06 -0700

    Shakapack Updates site live

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'employees/index'
   get 'employees/show'
+
+  root "employees#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
