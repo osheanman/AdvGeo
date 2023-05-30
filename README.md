007046ac5247bc47767b08eb475566ca498c36d9
Oshean Lehrmann <osheanman@berkeley.edu>
2023-05-30 12:58:59 -0700

    Adding Step Def

--- README.md
+++ README.md
@@ -9,10 +9,15 @@ ruby "3.2.2"
 
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 
-adding rspec and cucumber
+Testing Docs
+rspec Doc : http://rspec.info/
+Cucumber Doc : https://cucumber.io/docs/cucumber/
+Capybara Doc : https://github.com/teamcapybara/capybara
 
 # Possible additions: 
 gem 'figaro' gem 'date-validator' 
 testing: gem 'factory_bot' gem 'haml_lint' gem 'rubocop-rails' gem 'byebug' gem 'codecov' gem 'guard' * 
 dev: gem 'listen' gem 'web-console' gem 'spring' 
 prod: possible database: gem 'pg', '>= 0.18', '< 2.0' gem 'rails_12factor'
+
+
---
+++ features/step_definitions/navbar_steps.rb
@@ -0,0 +1,43 @@
+Given('I go to the homepage') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I see the Navbar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click on the Logo') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should be on the homepage') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click on the {string} button') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should be on the {string} page') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should be on the {string} index page') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should see the Navbar') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I hover on the {string} button') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I should see a button for {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('Click on the button for {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
